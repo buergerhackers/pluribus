2e2ddef45c3e91a5740bb0f1a197e8f7b9399838
Todd Bontrager <bontragertodd@gmail.com>
2016-05-01 13:21:34 -0400

    (feat) add evironment variable for database

--- server/db/dbconfig.js
+++ server/db/dbconfig.js
@@ -1,8 +1,16 @@
 /* Database Configuration */
 var mysql = require('mysql');
 var Sequelize = require('sequelize');
+var sequelize = null;
+
 //need something that chooses if it's local or on heroku
-var sequelize = new Sequelize("pluribus", "root", "");
+
+if (process.env.HEROKU_){
+  //initialize db on Heroku
+
+} else {
+  sequelize = new Sequelize("pluribus", "root", "");
+}
 
 //not sure if I need to export it
 module.exports = db;
