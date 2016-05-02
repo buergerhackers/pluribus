b68de783a8f280b5368c27d95a99a3e1f75f94af
Todd Bontrager <bontragertodd@gmail.com>
2016-05-02 17:36:50 -0400

    (feat) Basic outline schetch of schema

--- server/db/schemas.js
+++ server/db/schemas.js
@@ -1,1 +1,42 @@
 var sequelize = require('./db/dbconfig/sequelize');
+
+// we define the models we need using js--we don't need a schema file!
+var User = sequelize.define('User', {
+  firstName: Sequelize.STRING,
+  lastName: Sequelize.STRING,
+  email: Sequelize.STRING,
+  createdAt: Sequelize.DATE,
+  updatedAt: Sequelize.DATE
+});
+
+var Topic = sequelize.define('Topic', {
+  name: Sequelize.STRING,
+  createdAt: Sequelize.DATE,
+  updatedAt: Sequelize.DATE
+});
+
+var Plurb = sequelize.define('Plurb', {
+  text: Sequelize.STRING,
+  location: Sequelize.STRING,
+  createdAt: Sequelize.DATE,
+  updatedAt: Sequelize.DATE,
+  topicID: 'foreignID',
+  userID: 'foreignID',
+});
+
+//add relationships here
+//Message.belongsTo(User);
+// puts a UserId column on each Message instance
+// also gives us the `.setUser` method, available inside the .success callback
+// after creating a new instance of Message
+
+
+User.sync();
+Topic.sync();
+Plurb.sync();
+// creates these tables in MySQL if they don't already exist. Pass in {force: true}
+// to drop any existing user and message tables and make new ones.
+
+exports.User = User;
+exports.Topic = Topic;
+exports.Plurb = Plurb;
