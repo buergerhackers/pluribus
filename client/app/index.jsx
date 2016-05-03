43d4f668f70cd500948a80af20f0f3277d46196f
Mark Keith <mark.r.keith@gmail.com>
2016-05-02 20:02:46 -0700

    (feat) Add material ui into project with two simple examples

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -3,11 +3,20 @@ var ReactDOM = require('react-dom');
 var Map = require('./components/Map/Map.jsx');
 var Feed = require('./components/Feed/Feed.jsx');
 var Navbar = require('./components/Navbar/Navbar.jsx');
+var RaisedButton = require('material-ui/lib/raised-button');
+var TextField = require ('material-ui/lib/text-field');
+injectTapEventPlugin = require("react-tap-event-plugin");
+injectTapEventPlugin();
 
 var Main = React.createClass({
   render: function () {
     return (
       <div>Hello Freekin World!
+        <RaisedButton label="Default"/>
+        <TextField
+          hintText="Hint Text"
+        /><br/>
+          <br/>
         <Map />
         <Feed />
         <Navbar />
@@ -15,5 +24,6 @@ var Main = React.createClass({
     );
   },
 });
+
 
 ReactDOM.render(<Main />, document.getElementById('main'));
