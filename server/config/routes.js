bedbdfcaa4843139d070431bc479ae8fd085e844
Todd Bontrager <bontragertodd@gmail.com>
2016-05-03 19:01:29 -0400

    (fix) Fix merge conflict overwrite

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,1 +1,28 @@
-//routes go here
+var userController = require('../controllers/userController');
+var topicController = require('../controllers/topicController');
+var plurbController = require('../controllers/plurbController');
+
+/* Utilities */
+// handle errors and send response
+var sendResponse = function (res, err, data, status) {
+  if (err) {
+    res.status(400).send('Error: Record doesn\'t exist');
+  } else {
+    res.status(status).send(data);
+  }
+};
+
+module.exports = function (app) {
+
+  /* User Routes */
+  //Get all users
+  app.route('/api/route')
+    .get(userController.getAllUsers);
+
+
+  /* Topic Routes */
+  //Get all topics
+  
+  /* Plurb Routes */
+
+};
