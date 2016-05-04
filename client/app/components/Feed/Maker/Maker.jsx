64bb01a92daf5e5fe9af6b74fe70a7fc881a000a
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-05-04 04:19:20 -0400

    (feat) Integrate maker component

--- client/app/components/Feed/Maker/Maker.jsx
+++ client/app/components/Feed/Maker/Maker.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-const Maker = () => (
-  <div> Hello Super Sweet Maker </div>
-);
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import Colors from 'material-ui/styles/colors.js';
+
+// console.log(Colors);
+// const styles = {
+//   errorStyle: {
+//     color: Colors.orange500,
+//   },
+//   underlineStyle: {
+//     borderColor: Colors.orange500,
+//   },
+// };
+
+let Maker = React.createClass({
+  getInitialState() {
+    return { input: 'nothing' };
+  },
+  sendPlurb() {
+    console.log('sending a plurb');
+  },
+  render() {
+    return <MuiThemeProvider muiTheme={getMuiTheme()}>
+    <TextField
+      value={ this.props.message }
+      hintText="What is your opinion?"
+    />
+  </MuiThemeProvider>
+  }
+});
 
 export default Maker;
