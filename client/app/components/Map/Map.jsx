7a44b4cfcd65d89d89206e6b9f91c05075187b5f
Mark Keith <mark.r.keith@gmail.com>
2016-05-03 20:28:16 -0700

    (feat) Create GoogleMap class
    *Performed local testing to ensure map renders on the page

--- client/app/components/Map/Map.jsx
+++ client/app/components/Map/Map.jsx
@@ -1,11 +1,34 @@
-var React = require('react');
+import React from "react";
+import ReactDOM from 'react-dom';
 
-var Map = React.createClass({
-  render: function () {
+export default class GoogleMap extends React.Component {
+
+  componentDidMount(rootNode) {
+    var mapOptions = {
+      center: this.mapCenterLatLng(),
+      zoom: this.props.initialZoom,
+    },
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var marker = new google.maps.Marker({position: this.mapCenterLatLng(), title: 'Hi', map: map });
+    this.setState({ map: map });
+  }
+
+  mapCenterLatLng() {
+    var props = this.props;
+
+    return new google.maps.LatLng(props.mapCenterLat, props.mapCenterLng);
+  }
+
+  render() {
     return (
-      <div> Hello Super Sweet Map </div>
+      <div id='map' className='map-gic' style={{ height:'1000px', width:'1000px' }}></div>
     );
-  },
-});
+  }
+}
 
-module.exports = Map;
+// With ES6, getDefaultProps no longer works... had to define out of class constructor
+GoogleMap.defaultProps = {
+  initialZoom: 6,
+  mapCenterLat: 53.5333,
+  mapCenterLng: -113.4073126,
+};
