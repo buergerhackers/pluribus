f9fe5f5b6bfeb0e2cc9a1e080cad63c1f90c9290
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-05-04 04:17:51 -0400

    (feat) Integrate message component

--- client/app/components/Feed/Message/Message.jsx
+++ client/app/components/Feed/Message/Message.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import colors from 'material-ui/styles/colors';
 
+// const colors = styles.Colors;
+
+import Paper from 'material-ui/Paper';
 import List from 'material-ui/List';
 import ListItem from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
 import Subheader from 'material-ui/Subheader';
 
 class Message extends React.Component {
   render() {
     return <MuiThemeProvider muiTheme={getMuiTheme()}>
-      <ListItem primaryText={ this.props.message }>
+      <ListItem
+        leftAvatar={
+          <Avatar
+            color="orange"
+            backgroundColor="purple"
+          >
+            Plurb
+          </Avatar>
+        }
+      >
         { this.props.message }
       </ListItem>
     </MuiThemeProvider> 
