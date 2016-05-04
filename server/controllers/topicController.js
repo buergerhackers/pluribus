aebb0eb27004b1d745f24d109493ddc56bfb0e39
Todd Bontrager <bontragertodd@gmail.com>
2016-05-04 17:16:46 -0400

    (feat) Add createTopic method to topicController

--- server/controllers/topicController.js
+++ server/controllers/topicController.js
@@ -10,4 +10,16 @@ module.exports = {
       console.error(err);
     });
   },
+  createTopic: function (req, res) {
+    var topicData = {
+      name: req.body.name
+    };
+    Topic.create(topicData)
+    .then(function (topic) {
+      res.status(201).json(topic);
+    })
+    .catch(function (err) {
+      console.error (err);
+    });
+  },
 };
