95e4a2c95b32d083534f2b207742f7a032565a65
Todd Bontrager <bontragertodd@gmail.com>
2016-05-04 19:29:06 -0400

    (feat) Add getTopicByName and deleteTopic methods to topicController

--- server/controllers/topicController.js
+++ server/controllers/topicController.js
@@ -10,6 +10,7 @@ module.exports = {
       console.error(err);
     });
   },
+  
   createTopic: function (req, res) {
     var topicData = {
       name: req.body.name
@@ -17,6 +18,32 @@ module.exports = {
     Topic.create(topicData)
     .then(function (topic) {
       res.status(201).json(topic);
+    })
+    .catch(function (err) {
+      console.error (err);
+    });
+  },
+
+  getTopicByName: function (req, res) {
+    var topicName = req.params.topicName;
+    Topic.find({
+      where: {name: topicName}
+    })
+    .then(function (topic) {
+      res.status(200).json(topic);
+    })
+    .catch(function (err) {
+      console.error (err);
+    });
+  },
+
+  deleteTopic: function (req, res) {
+    var topicName = req.params.topicName;
+    Topic.destroy({
+      where: {name: topicName}
+    })
+    .then(function () {
+      res.status(201);
     })
     .catch(function (err) {
       console.error (err);
