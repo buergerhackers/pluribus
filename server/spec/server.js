e10d84b62997cd8d0c5ff204c76e6db621966b27
Todd Bontrager <bontragertodd@gmail.com>
2016-05-03 20:00:23 -0400

    (test) Add userController test and invalid route test

--- server/spec/server.js
+++ server/spec/server.js
@@ -1,9 +1,13 @@
 var path = require('path');
 var expect = require('chai').expect;
-
+var request = require('supertest');
+var Sequelize = require("sequelize");
+var User = require('../db/dbconfig').User;
+var userController = require('../controllers/userController');
 var server = require(path.join(__dirname, '..', './server.js'));
+var app = require('../server');
 
-describe('server()', function () {
+describe('server', function () {
   'use strict';
 
   it('exists', function () {
@@ -18,3 +22,21 @@ describe('server()', function () {
     expect(port).to.equal(3000);
   });
 });
+
+describe('Invalid Routes', function () {
+  // test that proper error code is recieved for invalid url
+  it('Should return error', function (done) {
+    request(app)
+    .get('/abc')
+    .expect(404)
+    .end(done);
+  });
+});
+
+describe('getAllUsers()', function () {
+  it('should be a function', function () {
+    expect(userController.getAllUsers).to.exist;
+    expect(userController.getAllUsers).to.be.a('function');
+  });
+});
+
