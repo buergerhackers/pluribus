99187198edc9ca1ad23e454c31a84ba2f3384688
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-05-04 22:37:01 -0400

    (feat) Improve searchbar filtering

--- client/app/components/Feed/Search/Search.jsx
+++ client/app/components/Feed/Search/Search.jsx
@@ -4,24 +4,39 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import TextField from 'material-ui/TextField';
 import AutoComplete from 'material-ui/AutoComplete';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
 
 // dummy store to be replaced with Redux Store
-let SearchStore = {getState: ""};
+let SearchStore = {getState: "Search topics", currentTopic: "", allTopics: ["burn", "bernie", "beluga", "bryan bierce", "blues brothers"]};
 
 class Search extends React.Component {
-  constructor() {
-    super();
-    this.state = SearchStore;
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: SearchStore.allTopics,
+    }
+  }
+  filter(input) {
+    console.log(this);
+    console.log('filtering input ...');
+    return this.state.dataSource.filter(this.filter(input));
+  }
+  _selectTopic(selected) {
+    console.log('SELECTED TOPIC -> backend API call');
   }
   render() {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme()}>
         <AutoComplete
+          hintText="Search topics"
           animated={true}
           anchorOrigin={{vertical: 'bottom', horizontal: 'left'}}
-          dataSource={["apple, banana, strawberry"]}
+          dataSource={ this.state.dataSource }
+          onUpdateInput={ this._refineMenu }
+          onNewRequest={ this._selectTopic }
         />
-      </MuiThemeProvider>
+      </MuiThemeProvider>  
     );
   }
 }
