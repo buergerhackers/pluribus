564c1f22bb461f0d988fa38988e7028304ab7748
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-05-05 03:58:07 -0400

    (feat) Imbue feed component with state

--- client/app/components/Feed/Feed.jsx
+++ client/app/components/Feed/Feed.jsx
@@ -13,11 +13,12 @@ import EyeGlass from 'material-ui/svg-icons/action/search';
 // dummy store to be replaced with Redux Store
 let FeedStore = {getState: "Feed"};
 
-class Feed extends React.Component {
-  constructor() {
-    super();
-    this._sendPlurb = this._sendPlurb;
-    this.state = FeedStore;
+export default class Feed extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = props;
+    console.log('Feed state:');
+    console.log(props);
   }
   sendPlurb() {
     console.log('sending plurb');
@@ -37,5 +38,3 @@ class Feed extends React.Component {
     </MuiThemeProvider>
   }
 }
-
-export default Feed;
