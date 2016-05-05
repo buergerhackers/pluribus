bbaae237a4cc871f7f47bed627d65d8472728a8e
Todd Bontrager <bontragertodd@gmail.com>
2016-05-04 22:58:54 -0400

    (feat) Add basic setup of OAuth

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,11 +1,36 @@
 var bodyParser = require('body-parser');
 var dotenv = require('dotenv');
+var session = require('express-session');
+var Grant = require('grant-express');
 
 if (!process.env.NODE_ENV) {
   dotenv.load();
 }
 
+/* New Grant For Google Authentication */
+var grant = new Grant({
+  server: {
+    protocol: 'http',
+    host: process.env.HOST || 'localhost:3000',
+    callback: '/callback',
+    transport: 'session',
+    state: true,
+  },
+  google: {
+    key: process.env.GOOGLE_CID,
+    secret: process.env.GOOGLE_CSECRET,
+    scope: [
+      'https://www.googleapis.com/auth/plus.login',
+      'https://www.googleapis.com/auth/plus.me',
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/userinfo.profile',
+    ],
+  },
+});
+
 module.exports = function (app, express) {
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
+  //TODO app.use(session...)
+  app.use(grant);
 };
