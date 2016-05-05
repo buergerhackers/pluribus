c2cb2bb70f64da3454eff643699c2738adafacbb
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-05-04 22:32:53 -0400

    (style) Update public and private filters
    * State reflects selected filter

--- client/app/components/Feed/Filter/Filter.jsx
+++ client/app/components/Feed/Filter/Filter.jsx
@@ -3,13 +3,10 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import { Tabs, Tab } from 'material-ui/Tabs';
-import RaisedButton from 'material-ui/RaisedButton';
 import Subheader from 'material-ui/Subheader';
 
-import SvgIcon from 'material-ui/SvgIcon';
-import Earth from 'material-ui/svg-icons/maps/terrain';
-import Pin from 'material-ui/svg-icons/maps/place';
-import Personal from 'material-ui/svg-icons/maps/person-pin';
+import Public from 'material-ui/svg-icons/social/public';
+import Private from 'material-ui/svg-icons/social/people';
 
 // dummy store to be replaced with Redux Store
 let FilterStore = {getState: 0};
@@ -17,23 +14,20 @@ let FilterStore = {getState: 0};
 class Filter extends React.Component {
   constructor() {
     super();
-    this._handleSwitch = this._handleSwitch;
     this.state = FilterStore;
   }
   _handleSwitch(type) {
-    this.setState({ filter: type });
+    this.setState({ getState: type });
     console.log('STATE:');
     console.log(this.state);
   }
   render() {
     return <MuiThemeProvider muiTheme={getMuiTheme()}>
       <Tabs 
-        value={this.state.filter}
-        onChange={this._handleSwitch}
+        value={this.state.getState}
       >
-        <Tab value={0} icon={<Earth />} />
-        <Tab value={1} icon={<Pin />} />
-        <Tab value={2} icon={<Personal />} />
+        <Tab value={0} icon={<Public />} onClick={ () => this._handleSwitch(0) } />
+        <Tab value={1} icon={<Private />} onClick={ () => this._handleSwitch(1) } />
       </Tabs>
     </MuiThemeProvider>
   }
