1525115e569fbcde704087beed3f5d0b3a6a3be5
Mark Keith <mark.r.keith@gmail.com>
2016-05-06 12:28:47 -0700

    (feat) Add ability for maker to make plurbs and dispatch data

--- client/app/components/Feed/Maker/Maker.jsx
+++ client/app/components/Feed/Maker/Maker.jsx
@@ -1,73 +1,70 @@
 import React from 'react';
 import { store } from '../../../STORE.jsx';
-console.log(store);
 import { createPlurb } from '../../../ACTIONS.jsx';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
-import Colors from 'material-ui/styles/colors.js';
 import ActionButton from 'material-ui/FloatingActionButton';
 import Plus from 'material-ui/svg-icons/content/add';
 
-// console.log(Colors);
-// const styles = {
-//   errorStyle: {
-//     color: Colors.orange500,
-//   },
-//   underlineStyle: {
-//     borderColor: Colors.orange500,
-//   },
-// };
-// listen for store changes!
-let unsubscribe = store.subscribe(() => console.log(store.getState()))
-
-class MakePlurb extends React.Component {
+export default class Maker extends React.Component {
   constructor() {
     super();
-    // this.state = props;
-  }
-  _sendPlurb() {
-    console.log(this);
-    // super._sendPlurb(this.state.message);
-    console.log("ACTION: 'CREATE_PLURB'");
-  }
-  render() {
-    return <MuiThemeProvider muiTheme={getMuiTheme()}>
-      <ActionButton 
-      primary={true}
-      children={<Plus />} 
-      onClick={ this._sendPlurb }
-      style={{ position: 'absolute', bottom: '50', right:'320' }}
-      />
-    </MuiThemeProvider>
-  }
-}
-
-export default class Maker extends React.Component {
-  constructor(props) {
-    super();
-    console.log(store.getState());
     this.state = {
-      message: ''
+      message: '',
+      location: '',
     };
+    this._getLocation();
+    this._updateMessage = this._updateMessage.bind(this);
+    this._sendPlurb = this._sendPlurb.bind(this);
+    this._getLocation = this._getLocation.bind(this);
   }
-  _sendPlurb(input) {
-    console.log("ACTION: 'CREATE_PLURB'");
-    console.log(input);
-    // send on return
-    input.keyCode === 13 ? store.dispatch(createPlurb(this.state.message, this.state.user, this.state.location)): false; 
-  }
-  render() {
-    return <div><MakePlurb /><TextField
-      value={ this.state.message }
-      hintText="What is your opinion?"
-      onKeyDown={this._sendPlurb}
-      type="text"
-      style={{ position: 'absolute', bottom: '50', right:'50' }}
-    />
-    </div>
+  _getLocation() {
+    let context = this;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      let pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+      context.setState({ location: pos });
+    }, function () {
+      console.log("Something Failed in grabbing geo coordinates");
+    });
   }
 
+  _sendPlurb() {
+    // console.log(
+    store.dispatch(createPlurb(null,
+      {
+        location: this.state.location,
+        message: this.state.message,
+      }
+    ));
+    // );
+
+    this.setState(
+      { message: '' }
+    );
+  }
+
+  _updateMessage(e) {
+    this.setState(
+      { message: e.target.value }
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <ActionButton
+          primary={true}
+          children={<Plus />} 
+          onClick={ this._sendPlurb }
+        />
+        <TextField
+          hintText="Have something to contribute?"
+          onChange= { this._updateMessage }
+          value= { this.state.message }
+        /><br/>
+      </div>
+    );
+  }
 }
