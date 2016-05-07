9865540b8150dff372010fddeecc1783dad6465e
Mark Keith <mark.r.keith@gmail.com>
2016-05-06 19:29:17 -0700

    (fix) Strip out unnecessary socket logs

--- server/server.js
+++ server/server.js
@@ -1,22 +1,21 @@
 var express = require('express');
 
+var Plurb = require('./controllers/plurbController');
+
 var app = express();
 
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
 var port = process.env.port || 3000;
-var server = app.listen(port, function() {
+var server = app.listen(port, function () {
   console.log('Server listening on port: ' + port);
 });
 
 var io = require('socket.io')(server);
 
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
   console.log('Socket connected!');
-  socket.on('createPlurb', function(data) {
-    console.log(data);
-  });
 });
 
 module.exports = app;
