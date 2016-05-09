16f1149fea9945e2807ca12bb006b0e6dfd25e6d
Todd Bontrager <bontragertodd@gmail.com>
2016-05-09 17:37:38 -0400

    (style) Remove and edit comments

--- server/db/dbconfig.js
+++ server/db/dbconfig.js
@@ -42,13 +42,10 @@ Topic.belongsToMany(User, {through: 'UserTopic'});
 
 //add one to many relationship between one user and many plurbs and one topic with many plurbs.
 Plurb.belongsTo(User);
-//Plurb.belongsTo(Topic);
+Plurb.belongsTo(Topic);
 
 // creates these tables in MySQL if they don't already exist. Pass in {force: true}
-// to drop any existing user and message tables and make new ones.
-//Plurb.sync();
-// User.sync();
-// Topic.sync();
+// to drop all existing tables and make new ones.
 sequelize.sync();
 
 exports.User = User;
