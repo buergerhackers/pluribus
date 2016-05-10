8a2fd7cc884d8a211963204b6e5df1358518e015
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-05-10 15:15:09 -0400

    (refactor) Include selected topic in plurbs query

--- client/app/components/Map/Map.jsx
+++ client/app/components/Map/Map.jsx
@@ -53,18 +53,19 @@ class GoogleMap extends React.Component {
       let Lats = newBounds.H;
       let Lngs = newBounds.j;
             
-      let currentBounds = {
+      let query = {
         mapBounds: {
           maxLat: +Lats.j.toFixed(2),
           maxLng: +Lngs.H.toFixed(2),
           minLat: +Lats.H.toFixed(2),
           minLng: +Lngs.j.toFixed(2)   
         }
+        topicId: this.props.currentTopicId
       }
       
       // update bounds on store, then re-fetch plurbs
       this.props.dispatch(updateMapBounds(currentBounds))
-      this.props.dispatch(getPlurbs(currentBounds))
+      this.props.dispatch(getPlurbs(query))
       
       // populate plurbs on map
       this.props.plurbs.map((plurb) => {
@@ -88,7 +89,8 @@ class GoogleMap extends React.Component {
 const mapStateToProps = (store) => {
   return {
     mapBounds: store.pluribusReducer.mapBounds,
-    plurbs: store.pluribusReducer.plurbs
+    plurbs: store.pluribusReducer.plurbs,
+    topicId: store.pluribusReducer.topicId
   };
 };
 
