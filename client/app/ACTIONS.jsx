c9affd5224f5647f017370077b698e134de859c6
Mark Keith <mark.r.keith@gmail.com>
2016-05-09 19:12:18 -0700

    (feat) Add topic related actions and reducers

--- client/app/ACTIONS.jsx
+++ client/app/ACTIONS.jsx
@@ -11,10 +11,26 @@ export const CREATE_PLURB = 'CREATE_PLURB';
 export const GET_PLURBS = 'GET_PLURBS';
 export const LOAD_PLURBS = 'LOAD_PLURBS';
 export const FETCHING = 'FETCHING';
+export const GET_TOPICS = 'GET_TOPICS';
+export const LOAD_TOPICS = 'LOAD_TOPICS';
 
 // CREATORS--------------------
 // sync
 export function selectTopic(currentTopic) {
+  fetch('/api/topic', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: currentTopic,
+    }),
+  }).then((topic) => {
+    store.dispatch(getTopics());
+  }).catch((error) => {
+    console.err(error);
+  });
   return { type: SELECT_TOPIC, currentTopic }
 }
 
@@ -74,4 +90,30 @@ export function loadPlurbs(plurbs) {
 
 export function fetching() {
   return { type: FETCHING, fetching: true };
+}
+
+export function getTopics() {
+  fetch('/api/topic', {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }).then((topics) => topics.text())
+      .then((topics) => {
+        store.dispatch(loadTopics(topics));
+    }).catch((error) => {
+      console.log("This is the error in getTopics",error);
+    });
+
+  return { type: GET_TOPICS, fetching: true };
+}
+
+export function loadTopics(topics) {
+  return { type: LOAD_TOPICS, fetching: false, topics }
+}
+
+export const FeedFilters = {
+  PUBLIC: 'PUBLIC',
+  PRIVATE: 'PRIVATE'
 }
