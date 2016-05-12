4081b52c267ed4bffabbf4d935c860e3e948ca20
Todd Bontrager <bontragertodd@gmail.com>
2016-05-12 19:24:11 -0400

    (fix) Add file extension

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack')
 module.exports = {
   devtool: 'source-map',
   entry: [
-    './client/app/index'
+    './client/app/index.jsx'
   ],
   output: {
     path: path.join(__dirname, 'public'),
