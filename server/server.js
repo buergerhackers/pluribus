2e158f34129e2cb5263811c9748667e0dc2bf2eb
Todd Bontrager <bontragertodd@gmail.com>
2016-05-13 14:16:57 -0400

    (fix) Change port number

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ var app = express();
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 var server = app.listen(port, function () {
   console.log('Server listening on port: ' + port);
 });
