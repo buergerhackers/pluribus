6707b37ce32d6db76712d2b5bf849a39cc512fff
Nicolas Matthew Vinson Wauquiez <nmvinson@gmail.com>
2016-05-14 18:04:24 -0400

    (feat) Introduce friend peek logic into default message state

--- client/app/components/Feed/Message/Message.jsx
+++ client/app/components/Feed/Message/Message.jsx
@@ -12,22 +12,47 @@ import { rePosition } from '../../Map/map_utils.jsx';
 export default class Message extends React.Component {
   constructor(props) {
     super(props);
+  
+    // identify default states of message
+    this.state = {
+      addFriend: false
+    }
+    console.log('default state:',this.state);
     this._reLoc = this._reLoc.bind(this);
+    this._friendPeek = this._friendPeek.bind(this);
+    // this._addFriend = this._addFriend.bind(this);
+
   }
   _reLoc() {
     // "snap" center of map to plurb location
     rePosition(this.props.plurb);
   }
+  _friendPeek() {
+    this.setState({
+      addFriend: true
+    });
+    console.log('addFriend:', this.state);
+    console.log('message item should transform to (leftAvatar: plus, primaryText: Name of Friend)');
+  }
   render() {
     let image = this.props.plurb.picture;
-    console.log('plurbId',this.props.plurb.id);
+    let friend = this.props.plurb.UserGoogid;
+    let text = this.props.plurb.text;
+    let name = this.props.plurb.firstName + ' ' + this.props.plurb.lastName;
+    if (this.state.addFriend) {
+      text = name;
+    }
     return (
       <Paper>
         <ListItem
-          leftAvatar={<Avatar 
-            onMouseEnter={ () => {console.log('Change to add friend state')}} 
-            onClick={ () => {console.log('Add friend!')} } src={image} />}
-          primaryText={ this.props.plurb.text }
+          leftAvatar={
+            <Avatar 
+              onMouseEnter={ this._friendPeek } 
+              onClick={ () => {console.log('Add friend (friendGoogId)', friend, this.props.plurb)} } 
+              src={image} 
+            />
+          }
+          primaryText={ text }
           rightIcon={<Pin onClick={ this._reLoc } />}
           style={{width: '96%'}}
         />
